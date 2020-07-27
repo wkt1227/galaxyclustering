d25b28f4d0bd694d0957aa0cb91248523aed79b0
Kazuki Tsuda <wkt1227@gmail.com>
2020-07-27 16:40:21 +0900

    Add make_galaxy_vectors.py

---
+++ src/make_galaxy_vectors.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+if __name__ == "__main__":
+    
+    patch_labels = np.load('../data/result/patch_labels.npy').astype(np.int64)
+    patch_labels2 = np.load('../data/result/patch_labels2.npy')
+    picked_crds = np.load('../data/result/picked_crds.npy')
+
+    galaxy_num = patch_labels.max()
+    class_num = patch_labels2.max()
+
+    galaxy_vectors = np.zeros((galaxy_num, class_num))
+    
+    for i in range(len(patch_labels)):
+        if patch_labels[i] == 0:
+            continue
+
+        galaxy_vectors[patch_labels[i]-1, patch_labels2[i]-1] += 1
+
+    # 正規化
+    s = galaxy_vectors.sum(axis=1, keepdims=True)
+    galaxy_vectors /= s
+
+    np.save('../data/result/galaxy_vectors.npy', galaxy_vectors)
